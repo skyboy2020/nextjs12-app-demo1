a28b73cdc25d0000722c716af50aa729ada4e49a
skyboy2020 <89569970@qq.com>
2022-03-15 18:06:05 +0800

    add getStaticProps in pages/idnex.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ articles }) {
+
+  console.log(articles)
+  
   return (
     <div>
       <Head>
@@ -13,4 +16,15 @@ export default function Home() {
       <h1>Welcome to Next</h1>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
