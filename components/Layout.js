ed9262267af2509a0b8f20b1ff586eecb48fcbd6
skyboy2020 <89569970@qq.com>
2022-03-15 16:25:51 +0800

    add styles/header.module.css

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,15 @@
-import Nav from './Nav'
-import styles from '../styles/Layout.module.css'
+import Nav from "./Nav";
+import styles from "../styles/Layout.module.css";
 
-const Layout = ( {children }) => {
-    return (
-        <>
-        <Nav/>
-        <div className={styles.container}>
-           
-            <main className={styles.main}>{children}</main>
-            
-        </div>
-        </>
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>{children}</main>
+      </div>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
