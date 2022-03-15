b2af1978211292d363262836a9a2ec1a8d495b96
skyboy2020 <89569970@qq.com>
2022-03-15 11:11:45 +0800

    add content on components/Layout.js

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Layout.module.css'
+
+const layout = () => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+
+            </main>
+        </div>
+    )
+}
+
+export default layout
