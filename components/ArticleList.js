a6cd9f50e162f863e63165077ed0530f354ad98e
skyboy2020 <89569970@qq.com>
2022-03-15 18:36:57 +0800

    add ArticleItem on components/ArticleList.js

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,11 +1,15 @@
 import articleStyles from '../styles/Article.module.css'
+import ArticleItem from './ArticleItem'
 
 
 const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
         {articles.map((article) => (
-           <h3 key={article.title}> {article.title} </h3>  
+            
+        //    <h3 key={article.title}> {article.title} </h3>  
+
+           <ArticleItem key={article.id}  article={article} />
         ))}
     </div>
   )
